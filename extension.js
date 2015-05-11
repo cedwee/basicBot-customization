6fd36ec432b9b63bb159ae9e11d7f2551b181d0c
cedwee <cedwee@gmail.com>
2015-05-12 03:17:01 +0800

    Update extension.js

--- extension.js
+++ extension.js
@@ -57,9 +57,9 @@
     localStorage.setItem("basicBotsettings", JSON.stringify({
         botName: "basicBot",
         language: "english",
-        startupCap: 1, // 1-200
-        startupVolume: 0, // 0-100
-        startupEmoji: false, // true or false
+        startupCap: 200, // 1-200
+        startupVolume: 100, // 0-100
+        startupEmoji: true, // true or false
         cmdDeletion: true,
         chatLink: "https://rawgit.com/cedwee/basicBot/master/lang/en.json",
         maximumAfk: 120,
